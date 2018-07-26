1c6f441b912fcf8f4eaa3db9441f624385d05853
James Lucktaylor <jlucktay+github@gmail.com>
2018-07-26 12:54:41 +0100

    Back to simpler times...

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 
 	"google.golang.org/appengine" // Required external App Engine library
 )
-
-var (
-	indexTemplate = template.Must(template.ParseFiles("index.html"))
-)
-
-type templateParams struct {
-	Notice string
-	Name   string
-}
 
 func main() {
 	http.HandleFunc("/", indexHandler)
@@ -32,33 +21,4 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// fmt.Fprintln(w, strings.Title(os.Getenv("GREETING")), "!")
-
-	params := templateParams{}
-
-	if r.Method == "GET" {
-		indexTemplate.Execute(w, params)
-		return
-	}
-
-	// It's a POST request, so handle the form submission.
-
-	name := r.FormValue("name")
-	params.Name = name // Preserve the name field.
-	if name == "" {
-		name = "Anonymous Gopher"
-	}
-
-	if r.FormValue("message") == "" {
-		w.WriteHeader(http.StatusBadRequest)
-
-		params.Notice = "No message provided"
-		indexTemplate.Execute(w, params)
-		return
-	}
-
-	// TODO: save the message into a database.
-
-	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", name)
-
-	indexTemplate.Execute(w, params)
 }
