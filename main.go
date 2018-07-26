deda80709d52865e4fadbddbdeddf6a48884239a
James Lucktaylor <jlucktay+github@gmail.com>
2018-07-26 09:48:29 +0100

    Went through some GCP App Engine tutorials

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"google.golang.org/appengine" // Required external App Engine library
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	appengine.Main() // Starts the server to receive requests
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// if statement redirects all invalid URLs to the root homepage.
+	// Ex: if URL is http://[YOUR_PROJECT_ID].appspot.com/FOO, it will be
+	// redirected to http://[YOUR_PROJECT_ID].appspot.com.
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
+	fmt.Fprintln(w, strings.Title(os.Getenv("GREETING")), "!")
+}
